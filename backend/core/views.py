f560adb174309125962da406a7cd91161afe0815
nabonsiniguse <nabonsin@gmail.com>
2026-07-04 20:40:43 +0300

    import necessary models

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets, permissions, filters 
+from django.contrib.auth.models import User 
+from .models import Employee, Task
+from .serializers import EmployeeSerializer, TaskSerializer, UserMiniSerializer
+ 
+  class EmployeeViewSet(viewSet.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+      
